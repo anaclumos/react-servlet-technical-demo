2c6eca24f4a073cfb1f877963b700d28d576d284
Sunghyun Cho <mail@chosunghyun.com>
2022-04-01 21:18:46 -0700

    Add App

--- react-frontend/src/pages/index.tsx
+++ react-frontend/src/pages/index.tsx
@@ -1,81 +1,56 @@
+import { stringify } from 'querystring'
 import React from 'react'
 import ServiceWrapper from '../components/ServiceWrapper'
 
 const Home = () => {
+  const [firstname, setFirstname] = React.useState('Sunghyun')
+  const [lastname, setLastname] = React.useState('Cho')
+  const [result, setResult] = React.useState('Empty')
+
   return (
     <ServiceWrapper>
-      <h1>Hello World</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat ex
-        sed leo condimentum ullamcorper. Vestibulum commodo ipsum elit, sed
-        fermentum nulla iaculis eu. Phasellus fermentum ipsum et vestibulum
-        ornare. Suspendisse aliquam mauris eget orci hendrerit, rutrum pharetra
-        diam dapibus. Donec faucibus nulla ac sem commodo, vel tincidunt metus
-        facilisis. Aliquam et ipsum non sapien dictum aliquam. Nam condimentum
-        porttitor libero ac efficitur. Nam mauris augue, rutrum in tortor non,
-        porta euismod mi. In hac habitasse platea dictumst. Mauris in diam et
-        lectus finibus sodales ac quis tellus. In lacinia felis in nisi varius
-        convallis. Quisque semper malesuada viverra. In hac habitasse platea
-        dictumst. Proin dignissim porta sollicitudin. Donec blandit eros sed
-        ante semper, sed rutrum nibh sagittis.
-      </p>
-      <p>
-        Quisque pretium turpis sit amet est ultrices mattis. Donec malesuada
-        eleifend sem, sit amet vulputate turpis ullamcorper vitae. Cras
-        fermentum, nisi suscipit tristique facilisis, felis erat dignissim
-        metus, ut congue justo lacus at urna. Duis sodales lacinia elit. Mauris
-        pretium orci nulla, ac dignissim elit sodales eu. Lorem ipsum dolor sit
-        amet, consectetur adipiscing elit. Vestibulum a malesuada mi, a
-        consectetur neque. Sed iaculis tellus vitae est fringilla tincidunt.
-        Vivamus efficitur eget tortor eget hendrerit. Quisque id lorem eget nibh
-        pellentesque eleifend. Ut at nunc ut augue vestibulum rutrum sit amet
-        non tellus. Vestibulum pulvinar ex ac neque mattis, a bibendum libero
-        feugiat. Ut urna odio, venenatis a nisi vestibulum, cursus imperdiet
-        nulla. Duis quam urna, varius sit amet aliquet nec, ullamcorper a quam.
-        Sed eu finibus urna, in pulvinar enim.
-      </p>
-
-      <p>
-        Nulla efficitur tortor scelerisque, sollicitudin justo eu, elementum
-        dolor. In pharetra sollicitudin volutpat. Donec consectetur non felis at
-        auctor. Phasellus sit amet sem id nunc luctus molestie vitae et sapien.
-        Nulla malesuada pretium cursus. Nam dapibus auctor ipsum, sit amet
-        pellentesque mauris aliquam in. Nunc ornare ut massa eu tristique. Morbi
-        sollicitudin, dolor quis aliquam venenatis, mauris ante pharetra nisl,
-        at pharetra ante nisi sollicitudin mi. Duis rutrum, dui sed ultrices
-        elementum, dui est varius metus, vitae viverra nulla tortor eu metus.
-      </p>
-
-      <p>
-        Aliquam quam sapien, laoreet vel nunc at, eleifend aliquet nisl. Orci
-        varius natoque penatibus et magnis dis parturient montes, nascetur
-        ridiculus mus. Orci varius natoque penatibus et magnis dis parturient
-        montes, nascetur ridiculus mus. Donec vel ante bibendum, tincidunt enim
-        eget, porta enim. Integer nec euismod erat. Mauris nec nisl ut dolor
-        dignissim volutpat vel eget tellus. Phasellus vel velit non lectus
-        pulvinar tristique vel quis tellus. Nunc eleifend tortor lacus, a
-        suscipit enim ultrices et. Donec venenatis, tortor eu consequat congue,
-        tortor nisl dapibus odio, sit amet molestie ipsum tellus in mauris.
-        Pellentesque ac sapien vel turpis lobortis fringilla. Morbi non tempus
-        magna. Aliquam erat volutpat. Suspendisse finibus tortor et luctus
-        fermentum. Vivamus at maximus neque. Vestibulum pretium accumsan neque,
-        id vulputate felis vulputate at. Vestibulum convallis tempor orci vitae
-        ornare.
-      </p>
-
-      <p>
-        Integer et magna congue, porttitor nisi nec, gravida metus. Nullam
-        tristique lectus at lacus vestibulum, ut facilisis lectus scelerisque.
-        Donec orci mauris, aliquet a eros nec, commodo consectetur turpis. Donec
-        vitae finibus magna, porta scelerisque lorem. Donec pellentesque
-        sagittis nibh vel mattis. Nunc tristique lorem a nisi tempor egestas.
-        Duis tempus, orci vel dignissim sollicitudin, odio turpis rutrum urna,
-        vitae suscipit felis velit nec augue. Duis dignissim nulla a sapien
-        aliquet, vitae posuere lorem laoreet. Suspendisse quis lectus nec dui
-        tempor rhoncus in sit amet turpis.
-      </p>
+      <h1>React + Java</h1>
+      <h2>Lookup User with Last name</h2>
+      <input
+        type="text"
+        placeholder="Last Name"
+        value={lastname}
+        onChange={(e) => setLastname(e.target.value)}
+      />
+      <button
+        onClick={() =>
+          requestBackendWithQuerystrings('lookupwithlastname', {
+            lastname,
+          }).then((data) => {
+            setResult(data)
+          })
+        }
+      >
+        Lookup User
+      </button>
+      <h2>Result</h2>
+      <pre>{result}</pre>
     </ServiceWrapper>
   )
+}
+
+const requestBackendWithQuerystrings = async (
+  pathname: RequestInfo,
+  query: Record<string, string>
+) => {
+  const path =
+    'http://localhost:8080/java-backend/' + pathname + '?' + stringify(query)
+  console.log('Requesting: ' + path)
+  const response = await fetch(path, {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    },
+  })
+  const data = await response.json()
+  return data
 }
 
 export default Home
